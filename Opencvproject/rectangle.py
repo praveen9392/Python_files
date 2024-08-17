a0ffbaf7e56b528299e7f6e779b304d83c5d8c93
praveen9392 <bpraveenkumark1@gmail.com>
2024-08-18 00:08:42 +0530

    rectangle file

--- Opencvproject/rectangle.py
+++ Opencvproject/rectangle.py
@@ -1,6 +1,13 @@
-import cv2 as cv
-img = cv.imread('Opencvproject/image.jpg')
-draw=cv.rectangle(img,(100,100),(110,110),(255,0,0),1)
-cv.imshow('imag', draw)
-cv.waitKey(0)
-cv.destroyAllWindows()
+import cv2
+
+# Load an image
+img = cv2.imread('Opencvproject/image.jpg')
+
+# Draw a bounding box around an object
+cv2.rectangle(img, (70, 50), (150, 150), (0, 255, 0), 2)
+
+# Display the output
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
